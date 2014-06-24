befe2c12334c508182f6d83016cd2932309c7b30
Dora Pinheiro <melisand@iol.pt>
2014-06-24 21:40:41 +0100

    first commit

---
+++ HelloWorld/src/App.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * My Java Application 
+ * @author Paulo Saraiva 
+ */
+public class App {
+	
+	/**
+	 * Application main
+	 * @param args - Application arguments
+	 */
+	public static void main(String[] args){
+		
+		//Print Hello
+		System.out.println("Hello");
+	}
+	
+}
